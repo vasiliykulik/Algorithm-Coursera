2c20a8f448a501d89411d28e116b52ef88909217
Vasiliy Kylik <detroit.vv@gmail.com>
2017-11-20 00:33:40 +0200

    Kd-tree PointSET

--- src/KdTree.java
+++ src/KdTree.java
@@ -6,6 +6,59 @@ import edu.princeton.cs.algs4.StdDraw;
 /**
  * Created by Vasiliy Kylik on 19.11.2017.
  */
+/*
+2d-tree implementation. Write a mutable data type KdTree.java that uses a 2d-tree to implement the same API (but replace
+PointSET with KdTree). A 2d-tree is a generalization of a BST to two-dimensional keys. The idea is to build a BST with
+points in the nodes, using the x- and y-coordinates of the points as keys in strictly alternating sequence.
+
+        Search and insert. The algorithms for search and insert are similar to those for BSTs, but at the root we use
+        the x-coordinate (if the point to be inserted has a smaller x-coordinate than the point at the root, go left;
+        otherwise go right); then at the next level, we use the y-coordinate (if the point to be inserted has a smaller
+        y-coordinate than the point in the node, go left; otherwise go right); then at the next level the x-coordinate,
+        and so forth.
+        Insert (0.7, 0.2)
+
+        insert (0.7, 0.2)
+        Insert (0.5, 0.4)
+
+        insert (0.5, 0.4)
+        Insert (0.2, 0.3)
+
+        insert (0.2, 0.3)
+        Insert (0.4, 0.7)
+
+        insert (0.4, 0.7)
+        Insert (0.9, 0.6)
+
+        insert (0.9, 0.6)
+        Insert (0.7, 0.2)
+        Insert (0.5, 0.4)
+        Insert (0.2, 0.3)
+        Insert (0.4, 0.7)
+        Insert (0.9, 0.6)
+        Draw. A 2d-tree divides the unit square in a simple way: all the points to the left of the root go in the left subtree;
+         all those to the right go in the right subtree; and so forth, recursively. Your draw() method should draw all of
+          the points to standard draw in black and the subdivisions in red (for vertical splits) and blue (for horizontal
+           splits). This method need not be efficient—it is primarily for debugging.
+        The prime advantage of a 2d-tree over a BST is that it supports efficient implementation of range search and
+        nearest-neighbor search. Each node corresponds to an axis-aligned rectangle in the unit square, which encloses
+        all of the points in its subtree. The root corresponds to the unit square; the left and right children of the
+        root corresponds to the two rectangles split by the x-coordinate of the point at the root; and so forth.
+
+        Range search. To find all points contained in a given query rectangle, start at the root and recursively
+        search for points in both subtrees using the following pruning rule: if the query rectangle does not intersect
+        the rectangle corresponding to a node, there is no need to explore that node (or its subtrees). A subtree is
+        searched only if it might contain a point contained in the query rectangle.
+        Nearest-neighbor search. To find a closest point to a given query point, start at the root and recursively
+        search in both subtrees using the following pruning rule: if the closest point discovered so far is closer
+        than the distance between the query point and the rectangle corresponding to a node, there is no need to
+        explore that node (or its subtrees). That is, search a node only only if it might contain a point that is
+        closer than the best one found so far. The effectiveness of the pruning rule depends on quickly finding a
+        nearby point. To do this, organize the recursive method so that when there are two possible subtrees to go
+        down, you always choose the subtree that is on the same side of the splitting line as the query point as the
+        first subtree to explore—the closest point found while exploring the first subtree may enable pruning of the
+        second subtree.
+*/
 public class KdTree {
     private int size = 0;
 
