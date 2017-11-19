90f1f8cef43096404b428babcf29ec854fb018c4
Vasiliy Kylik <detroit.vv@gmail.com>
2017-11-19 17:48:40 +0200

    Kd-tree

---
+++ src/w5geometricapplicationofbsts/BSTImplementation1dRangeCount.java
@@ -0,0 +1,11 @@
+package w5geometricapplicationofbsts;
+
+/**
+ * Created by Vasiliy Kylik on 18.11.2017.
+ */
+/*public class BSTImplementation1dRangeCount {
+    public int size(Key lo, Key hi) {
+        if (contains(hi)) return rank(hi) - rank(lo) + 1;
+        else return rank(hi) - rank(lo);
+    }
+}*/
---
+++ src/w5geometricapplicationofbsts/IntersectionImpl.java
@@ -0,0 +1,16 @@
+package w5geometricapplicationofbsts;
+
+/**
+ * Created by Vasiliy Kylik on 19.11.2017.
+ */
+/*public class IntersectionImpl {
+    Node x = root;
+    while(x!=null)
+    {
+        if(x.interval.intersects(lo,hi)) return x.interval;
+        else if(x.left == null) x=x.right;
+        else if(x.left.max<lo) x=x.right;
+        else x=x.left;
+    }
+    return null;
+}*/
