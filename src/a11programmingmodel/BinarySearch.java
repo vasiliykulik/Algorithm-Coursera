86f220cc3966b19b4633e6c91a51ee9c078d1534
Vasiliy Kylik <detroit.vv@gmail.com>
2018-03-26 03:51:26 +0300

    whitelist binarysearch

---
+++ src/a11programmingmodel/BinarySearch.java
@@ -0,0 +1,12 @@
+package a11programmingmodel;
+
+/**
+ * Created by Vasiliy Kylik (Lightning) on 26.03.2018.
+ */
+public class BinarySearch {
+    public BinarySearch() {
+    }
+    public static  int indexOf(int[]a, int key){
+
+    }
+}
