2e4bf794522f6cf9026df803cf7ce0bf657d8a75
Raketa <detroi.vv@gmail.com>
2020-10-01 09:24:45 +0300

    E22_StockMarketTrading

--- src/a25sortingapplications/E22_StockMarketTrading.java
+++ src/a25sortingapplications/E22_StockMarketTrading.java
@@ -3,5 +3,10 @@ package a25sortingapplications;
 /**
  * @author Vasiliy Kylik on(Rocket) on 01.10.2020.
  */
+
+/** Stock Market Trading
+ * Investors place buy and sell orders for a particular stock on an electronic exchange,
+ *
+ * */
 public class E22_StockMarketTrading {
 }
