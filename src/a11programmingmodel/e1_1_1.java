42598441a0fcbfebcddfcd4922054a5adc81a4dd
detroit.vv@gmail.com <vbnGHJ678R%^>
2021-09-13 15:22:54 +0300

    e1_1_1

---
+++ src/a11programmingmodel/e1_1_1.java
@@ -0,0 +1,12 @@
+package a11programmingmodel;
+
+public class e1_1_1 {
+    public static void main(String[] args) {
+        int a = (0+15)/2;
+        double b = 2e-6 * 100000000.1;
+        boolean c = true && false || true && true;
+        System.out.println("a= " +a);
+        System.out.println("b= " +b);
+        System.out.println("c= " +c);
+    }
+}
