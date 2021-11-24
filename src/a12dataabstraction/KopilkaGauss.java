91e0a3deb80d4395ed7eb8ed98e27ffc948fe110
detroit.vv@gmail.com <vbnGHJ678R%^>
2021-11-24 03:58:05 +0200

    KopilkaGauss

---
+++ src/a12dataabstraction/KopilkaGauss.java
@@ -0,0 +1,12 @@
+package a12dataabstraction;
+
+public class KopilkaGauss {
+    public static void main(String[] args) {
+        int end = 1000;
+        int copilka = 0;
+        for (int i=0;i<=end;i++){
+            copilka+=i;
+        }
+        System.out.println("copilka = " + copilka);
+    }
+}
